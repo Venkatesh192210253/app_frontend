faa31b03cecab906ddf1aa2d704be1886f94c361
Venkatesh192210253 <130165629+Venkatesh192210253@users.noreply.github.com>
2026-04-07 10:42:41 +0530

    Delete backend directory

--- backend/water/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import WaterIntake
-
-class WaterIntakeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WaterIntake
-        fields = ['date', 'glasses_count']
--- backend/water/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import WaterIntakeView
-
-urlpatterns = [
-    path('', WaterIntakeView.as_view(), name='water-get'),
-    path('update/', WaterIntakeView.as_view(), name='water-update'),
-]
